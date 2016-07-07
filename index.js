46de54c8af650da5a3bf59f6b48a06f98b308d0a
Blake Contreras <shardsofsilver@gmail.com>
2016-07-07 16:51:47 -0700

    [server: init] Serve index.html

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 1337;
 var app = express();
-var db = require('./app/config.js');
+var db = require('./app/config');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + '/public'));
 
-
+app.get('/', function(req, res) {
+  res.sendfile('index.html')
+})
 
 app.listen(port);
 console.log("Server listening on port " + port);
+
+module.exports = app;
