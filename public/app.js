1a5308f0cd98a2fc2dd9189f2b9ee4e2eef34930
George <georgejweiler@gmail.com>
2016-07-08 13:27:28 -0700

    [refactor] started angular ui routing (commented) refactored $http to recieve resp from server

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
-angular.module("app", ['ui.bootstrap'])
+angular.module("app", [
+  'ui.bootstrap',
+  'category'
+  // 'ui.router'
+  ])
+
+.config(function($httpProvider) { //add state and URL provide
+  
+//   $urlRouterProvider.otherwise('/')
+
+//   $stateProvider
+//     .state('/', {
+//       templateUrl: 'index.html',
+//       controller: 'appController'
+//     })
+//     .state('/test', {
+//       templateUrl: 'Category/category.html',
+//       controller: 'categoryController'
+//     })
+})
 
 .controller('appController', function($scope, $http, $document){
   $scope.state = "start"
@@ -14,7 +33,14 @@ angular.module("app", ['ui.bootstrap'])
     $scope.payload.category = target;
     $scope.payload.time = Date.now();
     $scope.payload.state = $scope.state
-    $http.post('/api/toggleActivity', $scope.payload)
+    $http({
+      method: 'POST',
+      url: '/api/toggleActivity'
+    }).then(function successCallback(response) {
+      console.log(response)
+    }, function errorCallback(err) {
+      console.error(err)
+    });
   }
 
   $scope.changeState = function(){
