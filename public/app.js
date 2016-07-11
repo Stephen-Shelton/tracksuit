d7c73fd85250fd0bdea8b737621fe546bd37c3e1
George <georgejweiler@gmail.com>
2016-07-11 14:24:07 -0700

    removed ui-routing. using ng-include to insert category.html instead

--- public/app.js
+++ public/app.js
@@ -4,23 +4,4 @@ angular.module("app", [
   'ui.router'
   ])
 
-.config(function($stateProvider, $urlRouterProvider, $httpProvider) { //add state and URL provide
-
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'index.html'
-    })
-    .state('home.category', {
-      url: "/category",
-      templateUrl: 'Category/category.html',
-      controller: 'categoryController'
-    })
-  
-  $urlRouterProvider.otherwise('/home/category')
-})
-
-
-
-
 
