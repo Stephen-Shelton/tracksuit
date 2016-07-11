b6a51409639841f4ddbdb294267f20c666f926ff
George <georgejweiler@gmail.com>
2016-07-11 14:26:40 -0700

    moved category html elements out of index and into a separate html file. Would like to modularize this more - reseraching how to use angular to do this. Each category has its own controller at the moment.

--- public/category/category.js
+++ public/category/category.js
@@ -1,47 +1,127 @@
-angular.module('category', [])
+var category = angular
 
-.controller('categoryController', function($scope, $http, $document, appFactory){
-  $scope.state = "start";
-  $scope.payload = {
-    category: "",
-    time: "",
-    state: ""
-  };
-   $scope.category = {
-      category : "filler text",
-      duration : "0 seconds"
+  .module('category', [])
+
+  .controller('categoryController', function($scope, $http, $document, categoryFactory){
+    ////////////////////////
+    /// Not sure if we even need this controller
+    ///////////////////////
+  })
+  .controller('sleepController', function($scope, $http, $document, categoryFactory){
+    $scope.activities = [
+      {
+      id: 1,
+      label: 'Nap'
+      },
+      {
+      id: 2,
+      label: 'Deep Sleep'
+      }
+    ];
+
+   $scope.categoryName = "Sleep"
+   $scope.state = "start";
+    $scope.payload = {
+      category: $scope.categoryName,
+      time: "",
+      state: ""
     };
-  $scope.sendData = appFactory.sendData;
+     $scope.category = {
+        category : "filler text",
+        duration : "0 seconds"
+      };
+    $scope.sendData = categoryFactory.sendData;
 
-  $scope.changeState = function(){
-    $scope.state === "start" ? $scope.state = "stop" : $scope.state = "start"
-  };
+    $scope.changeState = function(){
+      $scope.state === "start" ? $scope.state = "stop" : $scope.state = "start"
+    };
 
-  $scope.category = appFactory.category;
+    $scope.category = categoryFactory.category;
 
-})
-.factory('appFactory', ['$document', '$http', function($document, $http){
+  })
+  .controller('funController', function($scope, $http, $document, categoryFactory){
+   $scope.categoryName = "Fun"
+   $scope.state = "start";
+    $scope.payload = {
+      category: $scope.categoryName,
+      time: "",
+      state: ""
+    };
+     $scope.category = {
+        category : "filler text",
+        duration : "0 seconds"
+      };
+    $scope.sendData = categoryFactory.sendData;
 
-  var sendData = function(payload, state){
-    var target = document.getElementById('category').innerHTML
-    payload.category = target;
-    payload.time = Date.now();
-    payload.state = state
+    $scope.changeState = function(){
+      $scope.state === "start" ? $scope.state = "stop" : $scope.state = "start"
+    };
 
-   
+    $scope.category = categoryFactory.category;
 
-    $http.post('/api/toggleActivity', payload)
-    .then(function successCallback(response) {
-      category.category = response.data.category;
-      category.duration = response.data.duration;
-    }, function errorCallback(err) {
-      console.error(err)
-    });
-  }
+  })
+  .controller('workController', function($scope, $http, $document, categoryFactory){
+   $scope.categoryName = "work"
+   $scope.state = "start";
+    $scope.payload = {
+      category: $scope.categoryName,
+      time: "",
+      state: ""
+    };
+     $scope.category = {
+        category : "filler text",
+        duration : "0 seconds"
+      };
+    $scope.sendData = categoryFactory.sendData;
 
- 
+    $scope.changeState = function(){
+      $scope.state === "start" ? $scope.state = "stop" : $scope.state = "start"
+    };
 
-  return {
-    sendData : sendData
-  }
-}])
+    $scope.category = categoryFactory.category;
+
+  })
+  .controller('selfController', function($scope, $http, $document, categoryFactory){
+   $scope.state = "start";
+   $scope.categoryName = "Self Improvement"
+    $scope.payload = {
+      category: $scope.categoryName,
+      time: "",
+      state: ""
+    };
+     $scope.category = {
+        category : "filler text",
+        duration : "0 seconds"
+      };
+    $scope.sendData = categoryFactory.sendData;
+
+    $scope.changeState = function(){
+      $scope.state === "start" ? $scope.state = "stop" : $scope.state = "start"
+    };
+
+    $scope.category = categoryFactory.category;
+
+  })
+  .factory('categoryFactory', ['$document', '$http', function($document, $http){
+
+    var sendData = function(payload, state){
+      // console.log('sending data')
+      console.log('sending payload: ', payload)
+      // payload.category = target;
+      payload.time = Date.now();
+      payload.state = state
+
+      $http.post('/api/toggleActivity', payload)
+      .then(function successCallback(response) {
+        // console.log(response.data)
+        category.category = response.data.category;
+        category.duration = response.data.duration;
+      }, function errorCallback(err) {
+        console.error(err)
+      });
+    }
+
+    return {
+      sendData : sendData
+    }
+  }])
