47e9e69c70b0202ede1ced8161866e47582203c0
George <georgejweiler@gmail.com>
2016-07-11 10:21:43 -0700

    [ui: refactor] changing the hard coded category panel  html elements to Ui view templates

--- public/category/category.js
+++ public/category/category.js
@@ -1,4 +1,47 @@
 angular.module('category', [])
 
-.controller('categoryController', function($scope) {
-});
+.controller('categoryController', function($scope, $http, $document, appFactory){
+  $scope.state = "start";
+  $scope.payload = {
+    category: "",
+    time: "",
+    state: ""
+  };
+   $scope.category = {
+      category : "filler text",
+      duration : "0 seconds"
+    };
+  $scope.sendData = appFactory.sendData;
+
+  $scope.changeState = function(){
+    $scope.state === "start" ? $scope.state = "stop" : $scope.state = "start"
+  };
+
+  $scope.category = appFactory.category;
+
+})
+.factory('appFactory', ['$document', '$http', function($document, $http){
+
+  var sendData = function(payload, state){
+    var target = document.getElementById('category').innerHTML
+    payload.category = target;
+    payload.time = Date.now();
+    payload.state = state
+
+   
+
+    $http.post('/api/toggleActivity', payload)
+    .then(function successCallback(response) {
+      category.category = response.data.category;
+      category.duration = response.data.duration;
+    }, function errorCallback(err) {
+      console.error(err)
+    });
+  }
+
+ 
+
+  return {
+    sendData : sendData
+  }
+}])
