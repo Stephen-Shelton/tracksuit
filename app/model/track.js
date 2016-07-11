1a8ed87d2f62a69f493a1a0ce75baec548225153
gabriellei814 <yihuilei814@gmail.com>
2016-07-11 11:50:07 -0700

    [db: refactor] refactor the schema and passed the test

--- app/model/track.js
+++ app/model/track.js
@@ -1,10 +1,20 @@
 var db = require('../config');
 var mongoose = require('mongoose');
 
+var timeSchema = new mongoose.Schema({
+  start: Date,
+  stop: Date
+});
+
+var activitySchema = new mongoose.Schema({
+  name: String,
+  time: [timeSchema]
+});
+
 var trackSchema = new mongoose.Schema({
+  userID: String,
   category: String,
-  time: Date,
-  state: String
+  activities: [activitySchema]
 });
 
 var Track = mongoose.model('Tracker', trackSchema);
