21d31e885fa7e32ca9b191f3967850a3edad38a0
Blake Contreras <shardsofsilver@gmail.com>
2016-07-11 16:43:44 -0700

    Build out basic goal view and creation.

--- public/goals/goals.js
+++ public/goals/goals.js
@@ -7,11 +7,39 @@
 angular.module('goals', [])
 
 .controller('goalsController', ['$scope', function($scope) {
-  $scope.category = //factoryvar;
-  $scope.hours = 0;
+  $scope.time = goalsFactory.time;
+  $scope.categories = goalsFactory.categories;
+  $scope.goals = goalsFactory.goals;
+
+  $scope.createGoal = function(category, time) {
+    var goal = {
+      userID = 'testID', //TODO: Update this to username from global factoryvar
+      goalCategory = category,
+      goalActivityName = category, //TODO: Update this to activity from $scope
+      goalTime = time
+    };
+
+    $scope.goals[goal.goalActivityName] = goal;
+    return goal;
+  }
 
   $scope.postGoal = function($scope.category, $scope.hours) {
     // sendData
       //.then display posted data
   }
 }])
+
+.factory('goalsFactory', function() {
+  var categories = [
+    { name: 'Sleep' },
+    { name: 'Exercise' }
+  ];
+  var time = 0;
+  var goals = {};
+
+  return {
+    categories: categories,
+    time: time,
+    goals: goals
+  }
+})
