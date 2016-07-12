6915ad567d3309cc78c820487213ee51a1cce7c3
gabriellei814 <yihuilei814@gmail.com>
2016-07-12 12:48:44 -0700

    [db: refactor] change complete db doc to test db doc

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var mongodbUri = 'mongodb://gabe:0814@ds021994.mlab.com:21994/tracksuit-test';          //'mongodb://gabe:mks40@ds021994.mlab.com:21994/tracksuit';
+var mongodbUri = 'mongodb://gabe:mks40@ds021994.mlab.com:21994/tracksuit';//'mongodb://gabe:0814@ds021994.mlab.com:21994/tracksuit-test';          
 mongoose.connect(mongodbUri); //'mongodb://localhost/db');
 
 var db = mongoose.connection;
