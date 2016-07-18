39462b8f4475006f265d84d038d007099d531420
Sean Hunter <seanhunter@me.com>
2016-07-18 10:59:23 -0700

    fixed authentication routing

--- public/app.js
+++ public/app.js
@@ -7,52 +7,37 @@ angular.module("app", [
   'Auth'
 ])
 .config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/signin');
 
-  $stateProvider.state('goals', {
-    url: '/goals',
-    templateUrl: './goals/goals.html',
-    controller: 'goalsController',
-    authenticate: true
-  })
+  $urlRouterProvider.otherwise('/signin');
+  $stateProvider
+
   .state('signin', {
     url: '/signin',
     templateUrl: './auth/signin.html',
     authenticate: false
   })
-  // .state('category', {
-  //   url: '/',
-  //   templateUrl: './category/category.html',
-  //   controller: 'categoryController'
-  // })
   .state('dashboard', {
     url: '/',
     templateUrl: './dashboard/dashboard.html',
     controller: 'categoryController',
     authenticate: true
-  });
+  })
+  .state('goals', {
+    url: '/goals',
+    templateUrl: './goals/goals.html',
+    controller: 'goalsController',
+    authenticate: true
+  })
+
+  // .state('category', {
+  //   url: '/',
+  //   templateUrl: './category/category.html',
+  //   controller: 'categoryController'
+  // })
 })
 .run(['$rootScope', '$location', '$state', '$window', function ($rootScope, $location, $state, $window, sAuth) {
 
   $rootScope.user = {};
-
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    $rootScope.user.status = response.status;
-    // $rootScope.user.name = response.name;
-    // $rootScope.user.email = response.email;
-    console.log('LOOK FOR THIS NOW!!!!',response);
-    console.log('LOOK FOR THIS NOW!!!!',$rootScope.user);
-
-    if (response.status === 'connected') {
-      $state.go('dashboard');
-    } else if (response.status === 'not_authorized') {
-      $state.go('signin');
-    } else {
-      $state.go('signin');
-    }
-  }
 
   $window.fbAsyncInit = function() {
     FB.init({
@@ -67,18 +52,16 @@ angular.module("app", [
     FB.Event.subscribe('auth.authResponseChange', function(res) {
 
       if (res.status === 'connected') {
+        $rootScope.user = res;
+        console.log($rootScope.user)
+        console.log(res)
         FB.api('/me', function(res) {
-          $rootScope.$apply(function() {
-            $rootScope.user = res;
+          $rootScope.$apply(function(res) {
           });
         });
         $state.go('dashboard');
       } else {
-        FB.logout(function(res) {
-          $rootScope.$apply(function() {
-            $rootScope.user = {};
-          });
-        });
+        $rootScope.user = {};
         $state.go('signin');
       }
 
@@ -93,4 +76,14 @@ angular.module("app", [
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
+
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+    if(toState.authenticate && $rootScope.user.status !== 'connected') {
+      $state.transitionTo("signin");
+      event.preventDefault();
+    }
+  })
+
 }]);
+
+
